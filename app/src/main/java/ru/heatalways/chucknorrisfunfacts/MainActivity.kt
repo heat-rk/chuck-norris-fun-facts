70d8d07cf00492d9c0909e6bac20ef56cba9b04c
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-05-18 14:06:58 +0300

    #comment инициализация проекта

---
+++ app/src/main/java/ru/heatalways/chucknorrisfunfacts/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.heatalways.chucknorrisfunfacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
