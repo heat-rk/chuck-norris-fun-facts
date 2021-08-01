9afb4e29b62e47624323bbc9ffee7add7e30cbd1
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-08-01 12:58:15 +0400

    #comment интеграция тестирования корутин в тесты dao

--- app/src/androidTest/java/ru/heatalways/chucknorrisfunfacts/domain/database/dao/SavedJokesDaoTest.kt
+++ app/src/androidTest/java/ru/heatalways/chucknorrisfunfacts/domain/database/dao/SavedJokesDaoTest.kt
@@ -1,16 +1,23 @@
 package ru.heatalways.chucknorrisfunfacts.domain.database.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import ru.heatalways.chucknorrisfunfacts.data.entities.ChuckJoke
 import ru.heatalways.chucknorrisfunfacts.domain.database.AppDatabase
 import javax.inject.Inject
 import javax.inject.Named
 
+@ExperimentalCoroutinesApi
+@SmallTest
 @HiltAndroidTest
 class SavedJokesDaoTest {
     @get:Rule(order = 0)
@@ -37,7 +44,17 @@ class SavedJokesDaoTest {
     }
 
     @Test
-    fun test() {
+    fun testJokeInsertion_shouldHasOneJokeInDatabase() = runBlockingTest {
+        dao.insert(ChuckJoke(
+            categories = listOf("test", "fake"),
+            createdAt = null,
+            iconUrl = null,
+            id = "2",
+            updatedAt = null,
+            url = null,
+            value = "never gonna give you up"
+        ))
 
+        assertThat(dao.getAll()).hasSize(1)
     }
 }
