a740d82f691c9d0eb33782f13fb89c1580382472
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-08-25 14:21:45 +0400

    #comment фикс: добавлены диспатчеры к запросам в репозитории для шуток Чака

--- app/src/main/java/ru/heatalways/chucknorrisfunfacts/domain/repositories/chuck_norris_jokes/ChuckNorrisJokesRepositoryImpl.kt
+++ app/src/main/java/ru/heatalways/chucknorrisfunfacts/domain/repositories/chuck_norris_jokes/ChuckNorrisJokesRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package ru.heatalways.chucknorrisfunfacts.domain.repositories.chuck_norris_jokes
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 import ru.heatalways.chucknorrisfunfacts.data.database.AppDatabase
 import ru.heatalways.chucknorrisfunfacts.data.network.chuck_norris_jokes.ChuckNorrisJokesApi
@@ -10,56 +13,62 @@ import java.util.*
 
 class ChuckNorrisJokesRepositoryImpl(
     private val api: ChuckNorrisJokesApi,
-    private val appDatabase: AppDatabase
+    private val appDatabase: AppDatabase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ): ChuckNorrisJokesRepository {
 
-    override suspend fun random(category: String?): ApiResponse<ChuckJoke> {
+    override suspend fun random(category: String?) = withContext(dispatcher) {
         api.random(category).let { response ->
             if (response.isOk && response.value != null) {
-                return ApiResponse(Response.success(
+                return@withContext ApiResponse(Response.success(
                     response.value.toDomain()
                 ))
             }
-            return ApiResponse(response.error)
+            return@withContext ApiResponse(response.error)
         }
     }
 
-    override suspend fun categories(): ApiResponse<List<Category>> {
+
+    override suspend fun categories(): ApiResponse<List<Category>> = withContext(dispatcher) {
         api.categories().let { response ->
             if (response.isOk && response.value != null) {
-                return ApiResponse(Response.success(
+                return@withContext ApiResponse(Response.success(
                     response.value.map { Category.Specific(it) }
                 ))
             }
-            return ApiResponse(response.error)
+            return@withContext ApiResponse(response.error)
         }
     }
 
-    override suspend fun search(query: String): ApiResponse<List<ChuckJoke>> {
+
+    override suspend fun search(query: String) = withContext(dispatcher) {
         api.search(query).let { response ->
             if (response.isOk && response.value != null) {
-                return ApiResponse(Response.success(
+                return@withContext ApiResponse(Response.success(
                     response.value.result?.map { it.toDomain() }
                 ))
             }
-            return ApiResponse(response.error)
+            return@withContext ApiResponse(response.error)
         }
     }
 
-    override suspend fun saveJoke(joke: ChuckJoke) {
+    override suspend fun saveJoke(joke: ChuckJoke) = withContext(dispatcher) {
         appDatabase.savedJokesDao().insert(joke.toEntity().copy(
             savedAt = Calendar.getInstance().timeInMillis
         ))
     }
 
-    override suspend fun getSavedJokesBy(limit: Int, offset: Int): List<ChuckJoke> {
-        return appDatabase.savedJokesDao()
+    override suspend fun getSavedJokesBy(
+        limit: Int,
+        offset: Int
+    ) = withContext(dispatcher) {
+        return@withContext appDatabase.savedJokesDao()
             .getBy(limit, offset)
             .map { it.toDomain() }
     }
 
-    override suspend fun getAllSavedJokes(): List<ChuckJoke> {
-        return appDatabase.savedJokesDao()
+    override suspend fun getAllSavedJokes() = withContext(dispatcher) {
+        return@withContext appDatabase.savedJokesDao()
             .getAll()
             .map { it.toDomain() }
     }
