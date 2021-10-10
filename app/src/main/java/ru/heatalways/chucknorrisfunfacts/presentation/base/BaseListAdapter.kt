afe9f8c518ac3fd829d2c8cc59dec3ca4b5fbdb1
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-10-10 13:26:50 +0300

    #comment фикс: документация BaseListAdapter

--- app/src/main/java/ru/heatalways/chucknorrisfunfacts/presentation/base/BaseListAdapter.kt
+++ app/src/main/java/ru/heatalways/chucknorrisfunfacts/presentation/base/BaseListAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Abstract class, that "fixed" a problem with ListAdapter when you submit
- * one list (they have the same address) a few times and recycler view
+ * lists with the same addresses a few times and recycler view
  * doesn't update the view
  */
 abstract class BaseListAdapter<T, VH: RecyclerView.ViewHolder>(diffUtil: DiffUtil.ItemCallback<T>)
